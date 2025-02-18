ce389f809bff6b81a300aa4a523efafba93d31ad
UCardini <smit3cg@mail.uc.edu>
2025-02-18 15:47:38 -0500

    Hw 2 additions

--- wk_05/hw/hw_02/src/main.cpp
+++ wk_05/hw/hw_02/src/main.cpp
@@ -2,7 +2,22 @@
 
 int main()
 {
-    player roulett( 10 );
-    std::cout << roulett.spinWheel() << std::endl;
+    int playerBalance = 1000;
+    std::cout << "Welcome to the electronic Roulette wheel type of game!"
+              << std::endl;
+    while (true)
+    {
+        std::string choice = "";
+        std::cout
+            << "Would you like to make a bet (1 = yes), anything else for no: ";
+        std::cin >> choice;
+        bool cont = choice != "1";
+        if (playerBalance == 0 || cont)
+        {
+            std::cout << "Remaining Balance: " << playerBalance;
+            std::cout << "Thanks for playing!";
+            break;
+        }
+    }
     return 0;
 }
