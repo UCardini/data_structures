f3bc1411229f5b08d1a6e14dd26c516421cd1b96
UCardini <smit3cg@mail.uc.edu>
2025-02-19 23:47:03 -0500

    Minor formatting tweaks, needs error handling as well as other requirements

--- wk_06/lab/lab_05/src/main.cpp
+++ wk_06/lab/lab_05/src/main.cpp
@@ -7,48 +7,58 @@ int main()
 {
     shelf s;
     int entry;
-    while ( true )
+    while (true)
     {
         std::cout
-            << "Press 1 to add a movie to the shelf." << std::endl
-            << "Press 2 to remove a movie from the shelf." << std::endl
-            << "Press 3 to see how many movies are currently on the shelf."
+            << " ____________________________________________________________"
             << std::endl
-            << "Press 4 to quit." << std::endl;
+            << "|[Press 1] - to add a movie to the shelf.                    |"
+            << std::endl
+            << "|[Press 2] - to remove a movie from the shelf.               |"
+            << std::endl
+            << "|[Press 3] - to see how many movies are on the shelf.        |"
+            << std::endl
+            << "|[Press 4] - to quit.                                        |"
+            << std::endl
+            << "|____________________________________________________________|"
+            << std::endl;
+        std::cout << "\t\tType your selection: ";
         std::cin >> entry;
-        if ( entry == 1 )
+        std::cout << std::endl;
+        if (entry == 1)
         {
-            std::cin.ignore( std::numeric_limits<std::streamsize>::max(),
-                             '\n' );
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::string title = "";
             std::cout << "Enter the Title: ";
-            std::getline( std::cin, title );
-            std::cout << std::endl;
+            std::getline(std::cin, title);
 
             std::string description = "";
             std::cout << "Enter the Description: ";
-            std::getline( std::cin, description );
-            std::cout << std::endl;
+            std::getline(std::cin, description);
 
             std::string ageRating = "";
             std::cout << "Enter the Age Rating: ";
             std::cin >> ageRating;
             std::cout << std::endl;
 
-            movie p( title, description, ageRating );
-            s.shelfAdd( p );
-        }
-        else if ( entry == 2 )
-        {
-            movie p = s.shelfRemove();
+            movie p(title, description, ageRating);
+            s.shelfAdd(p);
+            std::cout << "\t\t[Added to Collection]" << std::endl;
             p.printMovie();
         }
-        else if ( entry == 3 )
+        else if (entry == 2)
+        {
+            movie p = s.shelfRemove();
+            std::cout << "\t\t[Removed from Collection]" << std::endl;
+            p.printMovie();
+        }
+        else if (entry == 3)
         {
             int f = s.getIndex();
-            std::cout << "Movies currently on shelf: " << f << std::endl;
+            std::cout << "\t\t[Movies currently in Collection: " << f << "]"
+                      << std::endl;
         }
-        else if ( entry == 4 )
+        else if (entry == 4)
         {
             break;
         }
